2df7387b2ae67c278cfbc8117163879b457b1b52
Anushk Jain <anushk2201@gmail.com>
2023-12-02 23:52:31 +0530

    Deleted The Extras And Created From Scratch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// First thing that we have to write is this two imports.
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Components are actually function which return renderable HTML, just like this App component
+function App() {
+  return <h1>Hello React!!!</h1>
+}
+
+// Now we use createRoot() method to create a root component in which all our component will render.
+// this root component is present in the index.html of public folder that we ger here using the document.getElementById() method.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// This will render our App component using the div with id root in the index.html.
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    // We can wrap the root component i.e. App in the <React.StrictMode> tag to enable the strict mode.
+    // The only thing it does is that during development it will render our components twice in order to find certain bugs and also check if we are using outdated parts of the React API.
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
